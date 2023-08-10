0ec2007e8f0b455cd560e210308e89628a315405
Juan Gustin <juandgustin@gmail.com>
2023-08-10 11:12:06 -0500

    Frontend Checkout Success Mailer - Working

--- frontend-teesa/src/views/Checkout/CheckoutSucess.jsx
+++ frontend-teesa/src/views/Checkout/CheckoutSucess.jsx
@@ -1,10 +1,7 @@
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import { useSelector } from 'react-redux';
 import waves from '../../assets/icon/waveschecklow.svg';
 import Button from '@mui/material/Button';
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import emailjs from '@emailjs/browser';
 
 const CheckoutSucess = () => {
   //R-R-D
@@ -16,28 +13,28 @@ const CheckoutSucess = () => {
 
   //Enviar Mail:
 
-  const allUser = useSelector((state) => state.userState);
+  // const allUser = useSelector((state) => state.userState);
 
-  useEffect(() => {
-    if (allUser.user) {
-      const user_name = allUser.userData.userName;
-      const user_email = allUser.userData.userEmail;
-      console.log(user_email);
-      emailjs
-        .send(
-          'service_2rp9duo',
-          'template_2y5jc5f',
-          { user_email, user_name },
-          'W5KJUGxF4wBdmUA3v'
-        )
-        .then((result) => {
-          console.log(result.text);
-        })
-        .catch((error) => {
-          console.log(error.text);
-        });
-    }
-  }, [allUser]);
+  // useEffect(() => {
+  //   if (allUser.user) {
+  //     const user_name = allUser.userData.userName;
+  //     const user_email = allUser.userData.userEmail;
+  //     console.log(user_email);
+  //     emailjs
+  //       .send(
+  //         'service_2rp9duo',
+  //         'template_2y5jc5f',
+  //         { user_email, user_name },
+  //         'W5KJUGxF4wBdmUA3v'
+  //       )
+  //       .then((result) => {
+  //         console.log(result.text);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error.text);
+  //       });
+  //   }
+  // }, [allUser]);
 
   return (
     <div
